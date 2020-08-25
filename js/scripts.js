eeed8911d6a28e5ae5c125f402f7c749e3fb2b04
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-25 16:32:20 -0700

    add function withdrawal amount to be added into bank account balance along with starting balance and deposit.
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>
    Co-Authored-By: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,21 @@ BankAccount.prototype.updateBalance = function() {
     const depositAmount = this.deposits[i].amount;
     depositTotal += depositAmount;
   }
-  this.balance = this.startingBalance + depositTotal;
+  let withdrawalTotal = 0;
+  for (i = 0; i < this.withdrawals.length; i++) {
+    const withdrawalAmount = this.withdrawals[i].amount;
+    withdrawalTotal += withdrawalAmount;
+  }
+  this.balance = this.startingBalance + depositTotal - withdrawalTotal;
 }
 
 BankAccount.prototype.addDeposit = function(deposit) {
   this.deposits.push(deposit);
+  this.updateBalance();
+}
+
+BankAccount.prototype.addWithdrawal = function(withdrawal) {
+  this.withdrawals.push(withdrawal);
   this.updateBalance();
 }
 
@@ -34,7 +44,8 @@ function Deposit(amount) {
 }
 
 function Withdrawal(amount) {
-  this.amount = amount
+  this.amount = amount,
+  this.timestamp = new Date();
 }
 
 // Bug to fix: if starting balance input is empty to start, it messes up our balance
@@ -46,17 +57,25 @@ $(document).ready(function() {
     $("#bank").submit(function(event) {
       event.preventDefault();
       // Gather inputs
-      let depositAmount = parseInt($("#depositAmount").val());
       let startingBalance = parseInt($("#initialDeposit").val());
+      let depositAmount = parseInt($("#depositAmount").val());
+      let withdrawalAmount = parseInt($("#withdrawalAmount").val());
       // Create bank account if doesn't exist
-      
       if (!created) {
         bankAccount = new BankAccount(startingBalance);
         created = true;
       }
       // Create new deposit and add to bank account
-      let newDeposit = new Deposit(depositAmount);
-      bankAccount.addDeposit(newDeposit);
+      if (depositAmount) {
+        let newDeposit = new Deposit(depositAmount);
+        bankAccount.addDeposit(newDeposit);
+      }
+      // Create new withdrawal and add to bank account
+      if (withdrawalAmount) {
+        let newWithdrawal = new Withdrawal(withdrawalAmount);
+        bankAccount.addWithdrawal(newWithdrawal);
+      }
+      // Check bank account object to see if it updated
       console.log(bankAccount);
     })
 })
