6e8d814b70d136071af24c741e5cb759655c86a1
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-25 16:20:31 -0700

    add function for starting balance to add deposite and return total of new balance.
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>
    Co-Authored-By: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Business Logic
-function BankAccount(balance) {
+function BankAccount(startingBalance) {
   this.deposits = [],
   this.withdrawals = [],
-  this.balance = balance;
+  this.startingBalance = startingBalance
 }
 
 /*
@@ -13,7 +13,13 @@ BankAccount.prototype.balance = function() {
 } */
 
 BankAccount.prototype.updateBalance = function() {
-  this.balance = this.deposits[this.deposits.length-1].amount;
+  //this.balance = this.deposits[this.deposits.length-1].amount;
+  let depositTotal = 0;
+  for (i = 0; i < this.deposits.length; i++) {
+    const depositAmount = this.deposits[i].amount;
+    depositTotal += depositAmount;
+  }
+  this.balance = this.startingBalance + depositTotal;
 }
 
 BankAccount.prototype.addDeposit = function(deposit) {
@@ -31,17 +37,26 @@ function Withdrawal(amount) {
   this.amount = amount
 }
 
-
-let bankAccount = new BankAccount(0);
+// Bug to fix: if starting balance input is empty to start, it messes up our balance
 
 //UI Logic
 $(document).ready(function() {
+    let created = false;
+    let bankAccount;
     $("#bank").submit(function(event) {
       event.preventDefault();
+      // Gather inputs
       let depositAmount = parseInt($("#depositAmount").val());
+      let startingBalance = parseInt($("#initialDeposit").val());
+      // Create bank account if doesn't exist
+      
+      if (!created) {
+        bankAccount = new BankAccount(startingBalance);
+        created = true;
+      }
+      // Create new deposit and add to bank account
       let newDeposit = new Deposit(depositAmount);
       bankAccount.addDeposit(newDeposit);
-      
       console.log(bankAccount);
     })
 })
